a6bc06d7bc7a3e6263cf408d9aac206d43d57321
Gurunandan R. Bhat <gbhat@pobox.com>
2013-05-15 13:18:05 +0530

    Initial DZilla template commit

---
+++ lib/Kelp/Module/SendFile.pm
@@ -0,0 +1,39 @@
+package Kelp::Module::SendFile;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Kelp::Module::SendFile - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Kelp::Module::SendFile;
+
+=head1 DESCRIPTION
+
+Kelp::Module::SendFile is
+
+=head1 AUTHOR
+
+Gurunandan R. Bhat E<lt>gbhat@pobox.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013- Gurunandan R. Bhat
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
